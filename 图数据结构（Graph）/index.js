6725ae95a94bd1ae9d34d6ea4a332da44c91e6b7
贺世浩 <heshihao1217@163.com>
2022-08-04 15:25:42 +0800

    补充

--- 图数据结构（Graph）/index.js
+++ 图数据结构（Graph）/index.js
@@ -22,5 +22,34 @@ Graph.prototype.addEdge = function (v, k) {
 }
 
 // 图的遍历（两种方法）
-// 深度优先搜索  DFS
-// 广度优先搜索  BFS
+// 深度优先搜索  DFS   数据结构->栈   将顶点存入栈中，顶点式沿着路径呗探索的，存在新的相邻顶点就去探索
+// 广度优先搜索  BFS   数据结构->队列  将顶点放入队列中，最先入队列的顶点最先被探索
+
+
+// 使用三种颜色区分顶点的状态
+// white  ：还没被访问
+// 灰色：被访问还没被探索
+// 黑色：被探索过
+
+// 广度优先搜索也就是  先横向找，在纵向找
+
+// 步骤1.创建一个队列Q
+// 将u标注为被发现的灰色，并且将v放入队列Q
+// 3.如果队列为空
+// (a)   将u从Q中出队列
+// （b）.将标注u为被发现的（灰色）；
+// （c） 将u所有未被访问过的邻点（白色）入队列；
+// （d） 将u标注为已被探索的（黑色）。
+
+let initColor = function () {
+  let color = [];
+  for (let i = 0; i < this.vertices.length; i++) {
+    color[this.vertices[i]] = 'white'
+  }
+  return color
+}
+
+Graph.prototype.BFS = function (v, callback) {
+  let color = initColor()
+}
+

9a79a1b2f7cbf6bd300b33de1a417a291ddf0c8f
heshihao <heshihao1217@163.com>
2022-10-08 21:54:20 +0800

    测试delete能否删除掉var声明的变量

--- 栈数据结构（stack）/index.js
+++ 栈数据结构（stack）/index.js
@@ -1,57 +1,54 @@
-'栈是一种后进先出的数据结构（LIFO）有序集合。新添加的元素或者待删除的元素都保存在栈的同一端，称之为栈顶，反之另一侧称之为栈底。新元素都靠近栈顶，旧元素在栈底。'
+"栈是一种后进先出的数据结构（LIFO）有序集合。新添加的元素或者待删除的元素都保存在栈的同一端，称之为栈顶，反之另一侧称之为栈底。新元素都靠近栈顶，旧元素在栈底。";
 
 function Stack() {
   // 用来存储栈内的元素
-  this.items = []
+  this.items = [];
 }
 
 // 新增元素
 Stack.prototype.push = function (element) {
-  this.items.push(element)
-}
+  this.items.push(element);
+
+  console.log("nihao");
+};
 
 // 移除栈顶的元素
 Stack.prototype.pop = function () {
-  return this.items.pop()
-}
+  return this.items.pop();
+};
 
 // 返回栈顶的元素
 Stack.prototype.peek = function () {
-  return this.items[this.items.length - 1]
-}
+  return this.items[this.items.length - 1];
+};
 
 // 判断栈是否为空
 Stack.prototype.isEmpty = function () {
-  return this.items.length == 0
-}
+  return this.items.length == 0;
+};
 
 // 清空栈内元素
 Stack.prototype.clear = function () {
-  this.items = []
-}
+  this.items = [];
+};
 
 // 返回栈内元素个数
 Stack.prototype.size = function () {
-  return this.items.length
-}
+  return this.items.length;
+};
 
 // 打印栈内元素
 Stack.prototype.print = function () {
-  console.log(this.items.toString())
-}
-
+  console.log(this.items.toString());
+};
 
 // Es6
 
 class Stack {
   constructor() {
-    this.items = []
+    this.items = [];
   }
-  push() {
-
-  };
-  pop() {
-
-  };
+  push() {}
+  pop() {}
   // ...
 }
